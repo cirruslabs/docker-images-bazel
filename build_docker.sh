c3913109f7f3eab3e7b070783ca1c81bbf5e3d67
Fedor Korotkov <fedor.korotkov@gmail.com>
2019-02-12 09:15:47 -0500

    Bazel 0.22.0

--- build_docker.sh
+++ build_docker.sh
@@ -3,4 +3,7 @@
 set -e
 
 docker build --tag cirrusci/bazel:base base
-docker build --tag cirrusci/bazel:0.16.1 --tag cirrusci/bazel:latest 0.16.1
+docker build --tag cirrusci/bazel:$BAZEL_VERSION \
+             --tag cirrusci/bazel:latest \
+             --build-arg bazel_version=$BAZEL_VERSION \
+             bazel
