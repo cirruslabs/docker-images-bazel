931e4c068d5b8aaee12311f26be6b16d8a253000
Fedor Korotkov <fedor@cirruslabs.org>
2022-01-29 21:55:39 -0500

    linux/arm64

--- build_docker.sh
+++ build_docker.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-docker build --tag cirrusci/bazel:base base
-docker build --tag cirrusci/bazel:$BAZEL_VERSION \
+docker buildx create --name multibuilder
+docker buildx use multibuilder
+docker buildx inspect --bootstrap
+
+docker buildx build --platform linux/amd64,linux/arm64 --tag cirrusci/bazel:base base
+docker buildx build --platform linux/amd64,linux/arm64 --tag cirrusci/bazel:$BAZEL_VERSION \
              --tag cirrusci/bazel:latest \
              --build-arg bazel_version=$BAZEL_VERSION \
              bazel
