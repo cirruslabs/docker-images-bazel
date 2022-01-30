a6fcc58652890ed4706d83dd26a99170bf9fd96e
Fedor Korotkov <fedor.korotkov@gmail.com>
2022-01-29 22:11:38 -0500

    Push via buildx

--- build_docker.sh
+++ build_docker.sh
@@ -6,8 +6,9 @@ docker buildx create --name multibuilder
 docker buildx use multibuilder
 docker buildx inspect --bootstrap
 
-docker buildx build --platform linux/amd64,linux/arm64 --tag cirrusci/bazel:base base
+docker buildx build --platform linux/amd64,linux/arm64 --tag cirrusci/bazel:base --push base
 docker buildx build --platform linux/amd64,linux/arm64 --tag cirrusci/bazel:$BAZEL_VERSION \
              --tag cirrusci/bazel:latest \
+             --push \
              --build-arg bazel_version=$BAZEL_VERSION \
              bazel
